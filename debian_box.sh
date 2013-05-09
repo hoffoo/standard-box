84f81bdd08cecf9dcb75b0419c6051bf4a3f5313
Marin <mgs@jpisoftware.net>
2013-05-09 14:02:36 -0700

    all the good vim plugins

--- debian_box.sh
+++ debian_box.sh
@@ -6,21 +6,26 @@ apt-get install --yes sudo vim curl git unzip screen build-essential tmux rsync 
 
 export EDITOR=vim
 echo 'UseDns no' >> /etc/ssh/sshd_config
-/etc/ini.d/ssh reload
-	
+/etc/init.d/ssh reload
 
-# setup vim with pathogen, vim-sensible, and basic vim setup
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-cd ~/.vim/bundle
-git clone https://github.com/tpope/vim-sensible.git
-mkdir ~/.vim/undo
 
 cd ~
 git clone https://github.com/hoffoo/standard-box
 /bin/cp -fb standard-box/tmux.conf .tmux.conf
 /bin/cp -fb standard-box/bashrc .bashrc
 /bin/cp -fb standard-box/vimrc .vimrc
+
+# setup vim with pathogen, vim-sensible, and basic vim setup
+mkdir -p ~/.vim/autoload ~/.vim/bundle ~/.vim/undo
+curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+cd ~/.vim/bundle
+git clone https://github.com/tpope/vim-sensible.git
+git clone https://github.com/tpope/vim-sleuth.git
+git clone https://github.com/tpope/vim-surround.git
+git clone https://github.com/ervandew/supertab.git
+git clone https://github.com/kien/ctrlp.vim.git
+git clone https://github.com/Townk/vim-autoclose.git
+
 
 # http shits
 # apt-get install lighttpd php5-cgi php5-common 
